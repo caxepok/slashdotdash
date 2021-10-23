3e4159d34fdebbb583b0aae073c28f7731e1962d
Aleksandr Gurianov <hello@golova.pro>
2021-10-24 02:31:52 +0500

    Add clear compare date button

--- src/slashdotdash/dashserver/ClientApp/src/pages/plan/plan-compare.jsx
+++ src/slashdotdash/dashserver/ClientApp/src/pages/plan/plan-compare.jsx
@@ -32,7 +32,7 @@ export const PlanCompare = () => {
           onChange={(value) => dispatch(setDate(value, "planCompare"))}
         />
       </Markup.DatePickerWrapper>
-      )
+      {date && <Clear onClick={() => dispatch(setDate(undefined, "planCompare"))} />})
     </Wrapper>
   );
 };
@@ -42,4 +42,27 @@ const Wrapper = styled.span`
   font-weight: 400;
   color: ${(p) => p.theme.colors.secondary};
   white-space: nowrap;
+`;
+
+export const Clear = styled.span`
+  width: 25px;
+  height: 25px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(p) => p.theme.colors.secondary};
+  border-radius: 25px;
+  cursor: pointer;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 24px;
+  margin-left: 0.2em;
+  background: ${(p) => p.theme.colors.quarternary}55;
+  &:hover {
+    color: ${(p) => p.theme.colors.accent};
+  }
+
+  &:before {
+    content: "×";
+  }
 `;
